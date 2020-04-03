3c08b80e8ed29714edeb05305a776ccabc7b9181
Gabriel Atanasiu <Gabriel.Atanasiu@viavisolutions.com>
2020-04-03 14:20:24 +0300

    C-004 - navigation-menu content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,9 @@ import { NavigationMenuComponent } from './navigation-menu/navigation-menu.compo
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
