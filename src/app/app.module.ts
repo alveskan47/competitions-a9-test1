fa6275776b73fb4b24c2a08b32b2a48461c214c4
Gabriel Atanasiu <Gabriel.Atanasiu@viavisolutions.com>
2020-04-04 19:52:36 +0300

    C-008 - routing implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,36 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 
+import { RouterModule } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CompetitionListComponent } from './competition-list/competition-list.component';
+import { TestPageComponent } from './test-page/test-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationMenuComponent
+    NavigationMenuComponent,
+    CompetitionListComponent,
+    LandingPageComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot([
+      { path: '', component: LandingPageComponent },
+      { path: 'competition-list', component: CompetitionListComponent },
+      { path: 'test-page', component: TestPageComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
